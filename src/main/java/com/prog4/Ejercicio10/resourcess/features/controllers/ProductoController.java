b523bb553539c8e375cfafdde330b41a88836b3b
nazareno11 <nazarenoluna86@gmail.com>
2026-04-29 10:39:33 -0300

    paginacion

--- src/main/java/com/prog4/Ejercicio10/resourcess/features/controllers/ProductoController.java
+++ src/main/java/com/prog4/Ejercicio10/resourcess/features/controllers/ProductoController.java
@@ -2,10 +2,14 @@ package com.prog4.Ejercicio10.resourcess.features.controllers;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.prog4.Ejercicio10.resourcess.features.model.Producto;
@@ -26,8 +30,13 @@ public class ProductoController {
         return repository.save(producto);
     }
 
-    @GetMapping
-    public List<Producto> listar() {
-        return repository.findAll();
+    @GetMapping("/paginado")
+    public Page<Producto> listarPaginado(
+            @RequestParam int page,
+            @RequestParam int size) {
+
+        Pageable pageable = PageRequest.of(page, size);
+
+        return repository.findAll(pageable);
     }
 }
--- src/main/java/com/prog4/Ejercicio10/resourcess/features/repositories/ProductoRepository.java
+++ src/main/java/com/prog4/Ejercicio10/resourcess/features/repositories/ProductoRepository.java
@@ -1,8 +1,10 @@
 package com.prog4.Ejercicio10.resourcess.features.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.prog4.Ejercicio10.resourcess.features.model.Producto;
 
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
+public interface ProductoRepository extends CrudRepository<Producto, Long>, PagingAndSortingRepository<Producto, Long> {
 }
