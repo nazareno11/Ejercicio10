cbb8f8ec1219f76ff13994c71738cf49c62e1754
nazareno11 <nazarenoluna86@gmail.com>
2026-05-20 10:54:16 -0300

    docker

--- src/main/java/com/prog4/Ejercicio10/Ejercicio10Application.java
+++ src/main/java/com/prog4/Ejercicio10/Ejercicio10Application.java
@@ -10,7 +10,9 @@ public class Ejercicio10Application {
 
 	public static void main(String[] args) {
 
-		Dotenv dotenv = Dotenv.load();
+		Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .load();
 		System.setProperty("DB_URL", dotenv.get("DB_URL"));
 		System.setProperty("DB_USER", dotenv.get("DB_USER"));
 		System.setProperty("DB_PASS",dotenv.get("DB_PASS"));
